a5cd59e8665558561fd498e8e6281b2fc591ad11
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-22 22:07:18 +0400

    setup scss, apply reset and necessery styles, create components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Map from './components/Map'
+import Results from './components/Results'
+import Search from './components/Search'
+import './scss/main.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main className='app'>
+			<h1 className='app__header'>IP Address Tracker</h1>
+			<Search />
+			<Results />
+			<Map />
+		</main>
+	)
 }
 
-export default App;
+export default App
