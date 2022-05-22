b4e76992e1aa75199661bfdc4404a4b3c94e3b48
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-22 22:32:51 +0400

    style search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import arrowIcon from '../images/icon-arrow.svg'
 
 const Search = () => {
-	return <div>Search</div>
+	return (
+		<div className='search'>
+			<input type='text' className='search__input' />
+			<button className='search__button'>
+				<img src={arrowIcon} alt='' />
+			</button>
+		</div>
+	)
 }
 
 export default Search
