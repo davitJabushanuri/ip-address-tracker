92f53003682cf77f4f3a63d2c5f83cad8f568f75
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-22 22:52:57 +0400

    style results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
 const Results = () => {
-	return <div>Results</div>
+	return (
+		<div className='results'>
+			<div className='results__container'>
+				<p className='results__container__header'>IP ADDRESS</p>
+				<h2 className='results__container__result'>192.212.174.101</h2>
+			</div>
+			<div className='results__container'>
+				<p className='results__container__header'>LOCATION</p>
+				<h2 className='results__container__result'>bROOKLYN NY</h2>
+			</div>
+			<div className='results__container'>
+				<p className='results__container__header'>TIMEZONE</p>
+				<h2 className='results__container__result'>UTC -5:00</h2>
+			</div>
+			<div className='results__container'>
+				<p className='results__container__header'>ISP</p>
+				<h2 className='results__container__result'>SpaceX Starlink</h2>
+			</div>
+		</div>
+	)
 }
 
 export default Results
