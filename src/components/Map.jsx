9f4f34b9e6e66fa36f44a118fb5a124c846b804e
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-24 01:48:48 +0400

    added missing files

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import customMarker from '../images/icon-location.svg'
